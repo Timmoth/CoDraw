3c03eff7359c7f9b1ac86177cad812acc398787a
Tim <t.jones@timmoth.com>
2022-05-01 15:17:28 +0100

    Added basic canvas pen drawing technique

--- CoDraw/CoDraw/Client/Pages/Index.razor.cs
+++ CoDraw/CoDraw/Client/Pages/Index.razor.cs
@@ -1,24 +1,95 @@
 ﻿using Blazor.Extensions;
 using Blazor.Extensions.Canvas.Canvas2D;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace CoDraw.Client.Pages
 {
     partial class Index : ComponentBase
     {
+        #region Properties
         private Canvas2DContext _context;
-
         protected BECanvasComponent _canvasReference;
+        private readonly List<List<(double x, double y)>> _lines = new();
+        private List<(double x, double y)>? _currentLine;
+        private (double x, double y)? _currentPos;
+        private readonly int _lineResolution = 2;
+        #endregion
+
 
         protected override async Task OnAfterRenderAsync(bool firstRender)
         {
-            this._context = await this._canvasReference.CreateCanvas2DAsync();
-            await this._context.SetFillStyleAsync("green");
+            if (firstRender)
+            {
+                _context = await _canvasReference.CreateCanvas2DAsync();
+            }
 
-            await this._context.FillRectAsync(10, 100, 100, 100);
+            await _context.BeginBatchAsync();
+            await _context.SetLineWidthAsync(2);
 
-            await this._context.SetFontAsync("48px serif");
-            await this._context.StrokeTextAsync("Hello Blazor!!!", 10, 100);
+            foreach (var line in _lines)
+            {
+                await _context.BeginPathAsync();
+                foreach (var (x, y) in line)
+                {
+                    await _context.LineToAsync(x, y);
+                }
+                
+                await _context.StrokeAsync();
+            }
+            await _context.EndBatchAsync();
         }
+
+        #region Events
+
+        public void MouseDown(MouseEventArgs e)
+        {
+            _currentPos = (e.OffsetX, e.OffsetY);
+            _currentLine = new List<(double x, double y)>()
+            {
+                _currentPos.Value
+            };
+            _lines.Add(_currentLine);
+
+        }
+
+        public void MouseUp(MouseEventArgs e)
+        {
+            _currentLine = null;
+            _currentPos = null;
+        }
+
+        public void MouseOut(MouseEventArgs e)
+        {
+            _currentLine = null;
+            _currentPos = null;
+        }
+
+        public void MouseMove(MouseEventArgs e)
+        {
+            if (_currentLine == null ||
+                _currentPos == null ||
+                Math.Abs(e.OffsetX - _currentPos.Value.x) < _lineResolution ||
+                Math.Abs(e.OffsetY - _currentPos.Value.y) < _lineResolution)
+            {
+                return;
+            }
+
+            _currentPos = (e.OffsetX, e.OffsetY);
+            _currentLine.Add(_currentPos.Value);
+        }
+
+        public void KeyDown(KeyboardEventArgs e)
+        {
+
+        }
+
+        public void KeyUp(KeyboardEventArgs e)
+        {
+
+        }
+
+        #endregion
+
     }
 }
